965be129321db599d13043fc7bdc50c841882b7a
MiniPasta <giaan01@uw.edu>
2022-02-20 20:45:24 -0800

    Netflix's ratings vs. year

--- chart 2.R
+++ chart 2.R
@@ -4,8 +4,17 @@ View(chart_2)
 library(ggplot2)
 library(dplyr)
 
+options(scipen = 999)
 
 ggplot(data = chart_2) +
-  geom_col(mapping = aes(x = rating, y = release_year, fill=type)) + ggtitle("Number of Netflix Shows and Movies Based on Ratings and Released Year")
+  geom_point(aes(x=rating, y = release_year)) + ggtitle("Netflix's ratings and released years")
 
-  
+#Purpose
+## The purpose of this chart is to allow us to see all the types of ratings that exist in Netflix's streaming service and
+## also compare it to the release year of that show or movie.
+
+#Insight
+## This chart compares the ratings to the year that the show or movie is released. We can see which type of rating Netflix 
+## favors and has more content on compared to other ratings. It also reveals what type of content might attract the audience
+## more to increase user friendly experience. This data set includes shows and movies that are released from the early 1920s
+## to the early 2020s with a variety of different ratings.
