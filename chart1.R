9b39a634d0827b796a3cd84a7366e00b81289021
LeoRen666 <2319277580@qq.com>
2022-02-21 18:23:36 -0800

    chart1“ git commit -m chart1“

---
+++ chart1.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+data <- read_csv("./archive/netflix_titles.csv")
+
+data %>% filter(type=="Movie") %>% group_by(director) %>% summarise(count=n()) %>%
+  arrange(desc(count)) %>%
+  na.omit() %>% head(10) %>%
+  ggplot(aes(x=reorder(director, count), y=count)) +
+  geom_col(fill="orange") +
+  coord_flip() +
+  labs(x="Director", y="Number of movies", 
+       title="Top 10 directors by number of movies")
